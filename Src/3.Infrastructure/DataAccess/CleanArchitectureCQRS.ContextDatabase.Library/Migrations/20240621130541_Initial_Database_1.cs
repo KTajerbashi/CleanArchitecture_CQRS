c38e2ef9c8c3d3abcac4452616b44ea1dea8c554
Tajerbashi <kamrantajerbashi@gmail.com>
2024-06-21 16:39:39 +0330

    .

---
+++ Src/3.Infrastructure/DataAccess/CleanArchitectureCQRS.ContextDatabase.Library/Migrations/20240621130541_Initial_Database_1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitectureCQRS.ContextDatabase.Library.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Database_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
