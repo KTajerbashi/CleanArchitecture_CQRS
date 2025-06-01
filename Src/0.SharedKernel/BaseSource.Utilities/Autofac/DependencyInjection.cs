0199248cc49b990855026d06f498d3aa09f20e71
KTajerbashi <kamrantajerbashi@gmail.com>
2025-06-01 11:04:43 +0330

    Feature : Autofac Added to Project

---
+++ Src/0.SharedKernel/BaseSource.Utilities/Autofac/DependencyInjection.cs
@@ -0,0 +1,51 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+
+namespace BaseSource.Utilities.Autofac;
+
+// Marker interfaces for lifetime management
+public interface IAutofacSingletonLifetime { }
+public interface IAutofacScopedLifetime { }
+public interface IAutofacTransientLifetime { }
+
+
+public static class DependencyInjection
+{
+    public static ContainerBuilder AddAutofacLifetimeServices(this ContainerBuilder builder)
+    {
+        // Get the assembly where your services are located
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        // Register services based on their lifetime interfaces
+        builder.RegisterAssemblyTypes(assembly)
+            .Where(t => t.IsAssignableTo<IAutofacSingletonLifetime>())
+            .AsImplementedInterfaces()
+            .SingleInstance();
+
+        builder.RegisterAssemblyTypes(assembly)
+            .Where(t => t.IsAssignableTo<IAutofacScopedLifetime>())
+            .AsImplementedInterfaces()
+            .InstancePerLifetimeScope();
+
+        builder.RegisterAssemblyTypes(assembly)
+            .Where(t => t.IsAssignableTo<IAutofacTransientLifetime>())
+            .AsImplementedInterfaces()
+            .InstancePerDependency();
+
+        return builder;
+    }
+
+    public static IServiceProvider BuildAutofacServiceProvider(this IServiceCollection services)
+    {
+        var builder = new ContainerBuilder();
+
+        // Populate Autofac with existing ASP.NET Core DI registrations
+        builder.Populate(services);
+
+        // Add our lifetime-based registrations
+        builder.AddAutofacLifetimeServices();
+
+        var container = builder.Build();
+        return new AutofacServiceProvider(container);
+    }
+}
